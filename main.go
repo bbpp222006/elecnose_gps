7ce43c1c12eb97ed87061ede4fa84724f1a9a854
bbpp222006 <34976166+bbpp222006@users.noreply.github.com>
2021-12-19 21:55:14 +0800

    upd

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
-import (
-    "fmt"
-    "github.com/imroc/req"
-	"github.com/gin-gonic/gin"
-)
+import "github.com/gin-gonic/gin"
 
 func main() {
-    e,_:=req.Get("http://www.baidu.com")
-    fmt.Println(e.String())
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
